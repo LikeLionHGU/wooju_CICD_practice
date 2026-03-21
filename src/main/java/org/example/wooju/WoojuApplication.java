0fb88b3b4a5faf8bb2bd717f5649335773e74e24
EastRain816 <dder2dder2@gmail.com>
2026-03-21 13:54:02 +0900

    Database setting changed

--- src/main/java/org/example/wooju/WoojuApplication.java
+++ src/main/java/org/example/wooju/WoojuApplication.java
@@ -2,8 +2,9 @@ package org.example.wooju;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WoojuApplication {
 
     public static void main(String[] args) {
